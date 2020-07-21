a5c2530b100a063474bc4d77c05c0c339e8081a1
Dmitry Sychev <axiumone@gmail.com>
2020-07-21 10:59:52 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates(:name, { :length => { :minimum => 2 } })
+    validates :email, uniqueness: true
 end
---
+++ app/validators/author_validator.rb
@@ -0,0 +1,3 @@
+class AuthorValidator < ActiveRecord::Validatior
+    
+end
